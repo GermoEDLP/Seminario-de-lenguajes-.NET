dde32813f2040fe761cf6d765053978501d298c8
German Brassini <german.brassini@gmail.com>
2021-03-13 00:26:08 -0300

    Ejercicio 11 - terminado

--- teoria1/Program.cs
+++ teoria1/Program.cs
@@ -240,11 +240,27 @@ namespace teoria1
             
             */
 
+            /*
+            EJERCICIO 10
+            ============
+            
             for(int i = 1; i <= 1000; i++){
                 if(i%17 == 0 || i%29==0){
                     Console.WriteLine(i);
                 }
             }
+            
+            */
+
+            Console.WriteLine("10/3 = " + 10 / 3);
+            Console.WriteLine("10.0/3 = " + 10.0 / 3);
+            Console.WriteLine("10/3.0 = " + 10 / 3.0);
+            int a = 10, b = 3;
+            Console.WriteLine("Si a y b son variables enteras, si a=10 y b=3");
+            Console.WriteLine("entonces a/b = " + a / b);
+            double c = 3;
+            Console.WriteLine("Si c es una variable double, c=3");
+            Console.WriteLine("entonces a/c = " + a / c);
 
 
 
