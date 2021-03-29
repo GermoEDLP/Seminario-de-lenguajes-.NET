69eb1610cd8e548f8a4b124606bf077ed76a569f
German Brassini <german.brassini@gmail.com>
2021-03-29 08:29:05 -0300

    Creación y prueba de practica 2

---
+++ practica2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace practica2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
